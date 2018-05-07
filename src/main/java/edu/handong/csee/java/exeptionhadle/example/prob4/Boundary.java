6e7e38af2141ebaa3bde09d0a214a1713ba67e55
eory96 <21500057@handong.edu>
2018-05-07 21:29:34 +0900

    fourth comment add

--- src/main/java/edu/handong/csee/java/exeptionhadle/example/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/exeptionhadle/example/prob4/Boundary.java
@@ -1,12 +1,12 @@
-package edu.handong.csee.java.exeptionhadle.example.prob4;
+package edu.handong.csee.java.exeptionhadle.example.prob4;//package name
 
-public class Boundary {
-	private int [] arr = new int[5];
-	private int cnt = 0;
-	public void receive(int num) throws ArrayIndexOutOfBoundsException{
-		int i = cnt;
-		arr[i] = num;
-		System.out.printf("arr[%d] <- %d\n",i,num);
-		cnt++;
+public class Boundary {//this is Boundary class for save array and throw to ArrayIndexOutOfBoundsException
+	private int [] arr = new int[5];//declare new array
+	private int cnt = 0;//declare cnt for count number
+	public void receive(int num) throws ArrayIndexOutOfBoundsException{//this method is parameter type is int and throw to ArrayIndexOutOfBoundsException
+		int i = cnt;//declare i
+		arr[i] = num;//save array element 0 to 4
+		System.out.printf("arr[%d] <- %d\n",i,num);//printout "arr[%d] <- %d\n",i,num
+		cnt++;//increase 1 cnt
 	}
 }
--- src/main/java/edu/handong/csee/java/exeptionhadle/example/prob4/BoundaryMain.java
+++ src/main/java/edu/handong/csee/java/exeptionhadle/example/prob4/BoundaryMain.java
@@ -1,21 +1,21 @@
-package edu.handong.csee.java.exeptionhadle.example.prob4;
-import java.util.Scanner;
+package edu.handong.csee.java.exeptionhadle.example.prob4;//package name
+import java.util.Scanner;//use for keyboard
 
-public class BoundaryMain {
+public class BoundaryMain {//this is main class
 
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		Boundary arr = new Boundary();
-		int num =0;
-		for(int i=0;;i++) {
-			try{
-				System.out.print("Enter an integer : ");
-				num=keyboard.nextInt();
-				arr.receive(num);
+	public static void main(String[] args) {//this is main method
+		Scanner keyboard = new Scanner(System.in);//receive data from keyboard
+		Boundary arr = new Boundary();//instantiation Boundary class to arr
+		int num =0;//declare num
+		for(int i=0;;i++) {//fill out array element
+			try{//first implement this block
+				System.out.print("Enter an integer : ");//printout "Enter an integer : "
+				num=keyboard.nextInt();//receive data from keyboard
+				arr.receive(num);//send a num value to receive method 
 			}
-			catch(ArrayIndexOutOfBoundsException e) {
-				System.out.println("Invalid array index access!");
-				break;
+			catch(ArrayIndexOutOfBoundsException e) {//catch ArrayIndexOutOfBoundsException class type e
+				System.out.println("Invalid array index access!");//printout "Invalid array index access!"
+				break;//escaping loop
 			}
 		}
 	}
