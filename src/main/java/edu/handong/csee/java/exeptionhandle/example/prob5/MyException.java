6d3e911c0ba8702f160afdda9cf0d08dea7ca141
eory96 <21500057@handong.edu>
2018-05-07 21:50:58 +0900

    fifth comment add

--- src/main/java/edu/handong/csee/java/exeptionhandle/example/prob5/MyException.java
+++ src/main/java/edu/handong/csee/java/exeptionhandle/example/prob5/MyException.java
@@ -1,14 +1,14 @@
-package edu.handong.csee.java.exeptionhandle.example.prob5;
+package edu.handong.csee.java.exeptionhandle.example.prob5;//package name
 
-public class MyException extends Exception{
-	private String message = null;
+public class MyException extends Exception{//this class is son class of Exception class
+	private String message = null;//declare message variable
 	
-	public MyException(String mse) {
-		super(mse);
-		this.message = mse;
+	public MyException(String mse) {//this is constructor of this class
+		super(mse);//send mse value to Exception class
+		this.message = mse;//this message is mse
 	}
 	
-	public String toString() {
-		return message;
+	public String toString() {//this method return String type
+		return message;//rteurn message value
 	}
 }
--- src/main/java/edu/handong/csee/java/exeptionhandle/example/prob5/MyMain.java
+++ src/main/java/edu/handong/csee/java/exeptionhandle/example/prob5/MyMain.java
@@ -1,27 +1,27 @@
-package edu.handong.csee.java.exeptionhandle.example.prob5;
+package edu.handong.csee.java.exeptionhandle.example.prob5;//package name
 
-import java.util.Scanner;
+import java.util.Scanner;//use for keyboard
 
-public class MyMain {
+public class MyMain {//this is main class
 
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		String str = null;
-		try {
-			str = keyboard.nextLine();
-			MyMain.myTest(str);
+	public static void main(String[] args) {//this is main method
+		Scanner keyboard = new Scanner(System.in);//using for keyboard
+		String str = null;//declare str that type is stirng
+		try {//first implement this block
+			str = keyboard.nextLine();//input data to str
+			MyMain.myTest(str);//sent str value to myTest method of MyMain class 
 		}
-		catch(MyException a){
-			System.out.println("Inside catch block : "+a);
+		catch(MyException a){//catch MyExceoption class type 
+			System.out.println("Inside catch block : "+a);//print out "Inside catch block : "+a
 		}
 	}
 	
-	static void myTest(String b) throws MyException{
-		if(b.equals("null")) {
-			throw new MyException("String val is null");
+	static void myTest(String b) throws MyException{//this is static method and parameter type is strng, throw to MyException that process error message
+		if(b.equals("null")) {//if value of b is "null"
+			throw new MyException("String val is null");//printout "String val is null"
 		}
-		else
-			System.out.println("String val is " + b);
+		else//if value of b is not "null"
+			System.out.println("String val is " + b);//printout "String val is " + b
 	}
 
 }
