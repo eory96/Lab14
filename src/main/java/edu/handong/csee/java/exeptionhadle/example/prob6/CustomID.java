03565facc93fbd8222878db75b51655b11c5a2e0
eory96 <21500057@handong.edu>
2018-05-07 22:53:39 +0900

    last commit for comment

--- src/main/java/edu/handong/csee/java/exeptionhadle/example/prob6/CustomID.java
+++ src/main/java/edu/handong/csee/java/exeptionhadle/example/prob6/CustomID.java
@@ -1,53 +1,53 @@
-package edu.handong.csee.java.exeptionhadle.example.prob6;
+package edu.handong.csee.java.exeptionhadle.example.prob6;//this is package name
 
-public class CustomID {
-	private String name;
-	private int age;
-	private String race;
-	private int state;
-	private String[] races = new String[] {"Vulcans","Romulan","Klingons"};
+public class CustomID {//this is CustomID class
+	private String name;//this variable will save name
+	private int age;//this variable will save age
+	private String race;//this variable will save race
+	private int state;//this variable will save state
+	private String[] races = new String[] {"Vulcans","Romulan","Klingons"};//declare new array and fill element
 	
-	public void nameSetting(String name)throws Exception {
-		this.name = name;
-		if(name.length()<5) {
-			throw new Exception("Your name is short! Try again!");
+	public void nameSetting(String name)throws Exception {//this method is get name and error will processed by Exception
+		this.name = name;//this class name receive main class name
+		if(name.length()<5) {//if name length is shorter than 5 
+			throw new Exception("Your name is short! Try again!");//throw new error message "Your name is short! Try again!"
 		}
-		else {
-			System.out.println("Name is valid");
-			System.out.println("Name : "+ name);
-			state++;
+		else {//if name length is longer than 5
+			System.out.println("Name is valid");//printout "Name is valid"
+			System.out.println("Name : "+ name);//printout "Name : "+ name
+			state++;//increase state value
 		}
 	}
 	
-	public void ageSetting(int age) throws Exception{
-		this.age=age;
-		if(this.age<18) {
-			state = 1;
-			throw new Exception("You are too young!, Try again!");
+	public void ageSetting(int age) throws Exception{//this method for get age and error will processed by Exception
+		this.age=age;//this class age variable get main class age value
+		if(this.age<18) {//if age value is less than 18
+			state = 1;//set a state value to 1
+			throw new Exception("You are too young!, Try again!");//throw new error message "You are too young!, Try again!"
 		}
-		else {
-			System.out.println("Age is valid");
-			System.out.println("Age : "+ age);
-			state++;
+		else {//if age value is more than 18
+			System.out.println("Age is valid");//printout "Age is valid"
+			System.out.println("Age : "+ age);//printout "Age : " +age
+			state++;//increase state value
 		}
 	}
 	
-	public void raceSetting(String race) throws Exception{
-		this.race=race;
-		for(int i=0;i<3;i++) {
-			if(races[i].equals(this.race)) {
-				System.out.println("Race is valid");
-				System.out.println("Race : "+this.race);
-				state=0;
-				return ;
+	public void raceSetting(String race) throws Exception{//this method for compare race and error will processed by Exception
+		this.race=race;//this class race variable get main class race value
+		for(int i=0;i<3;i++) {//loop for compare race 
+			if(races[i].equals(this.race)) {//if race array has a race value 
+				System.out.println("Race is valid");//printout "Race is valid"
+				System.out.println("Race : "+this.race);//printout "Race : "+this.race
+				state=0;//set state 0 and begin from the first
+				return ;//escaping this method
 			}
 		}
-		state=2;
-		throw new Exception("Human! Try again.");
+		state=2;//set state 2 and repeat get race
+		throw new Exception("Human! Try again.");//throw new error message "Human! Try again."
 	}
 	
-	public int getState() {
-		return state;
+	public int getState() {//this method is return int type and determine to use method
+		return state;//return state value
 	}
 
 	
--- src/main/java/edu/handong/csee/java/exeptionhadle/example/prob6/CustomMain.java
+++ src/main/java/edu/handong/csee/java/exeptionhadle/example/prob6/CustomMain.java
@@ -1,35 +1,35 @@
-package edu.handong.csee.java.exeptionhadle.example.prob6;
-import java.util.Scanner;
+package edu.handong.csee.java.exeptionhadle.example.prob6;//package name
+import java.util.Scanner;//using for keyboard
 
-public class CustomMain {
+public class CustomMain {//this is main class
 
-	public static void main(String[] args) {
-		String name;
-		String race;
-		int age =0;
-		Scanner keyboard = new Scanner(System.in);
-		CustomID information = new CustomID();
+	public static void main(String[] args) {//this is main method
+		String name=null;//declare name variable
+		String race=null;//declare race variable
+		int age =0;//declare age variable
+		Scanner keyboard = new Scanner(System.in);//input data from keyboard
+		CustomID information = new CustomID();//instantiation CustomID to information
 		
-		for(;;) {
-			try {
-				if(information.getState()==0) {
-					System.out.print("Enter your name: ");
-					name = keyboard.nextLine();
-					information.nameSetting(name);
+		for(;;) {//infinite loop
+			try {//first implement this block
+				if(information.getState()==0) {//if information.getState() is 0 implement this block
+					System.out.print("Enter your name: ");//printout "Enter your name: "
+					name = keyboard.nextLine();//save data about name
+					information.nameSetting(name);//send name value to nameSetting method
 				}
-				else if(information.getState()==1) {
-					System.out.print("Enter your age: ");
-					age = keyboard.nextInt();
-					information.ageSetting(age);
+				else if(information.getState()==1) {//if information.getState() is 1 implement this block
+					System.out.print("Enter your age: ");//printout "Enter your age: "
+					age = keyboard.nextInt();//save data about age
+					information.ageSetting(age);//send age value to ageSetting method
 				}
-				else if(information.getState()==2) {
-					System.out.print("Enter your race: ");
-					race = keyboard.nextLine();
-					information.raceSetting(race);
+				else if(information.getState()==2) {//if information.getState() is 2 implement this block
+					System.out.print("Enter your race: ");//printout "Enter your race: "
+					race = keyboard.nextLine();//save data about race
+					information.raceSetting(race);//send race value to raceSetting method
 				}
 			}
-			catch(Exception e) {
-				System.out.println(e.getMessage());
+			catch(Exception e) {//catch Exception class type
+				System.out.println(e.getMessage());//printout error message
 			}
 		}
 	}
